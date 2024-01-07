4c3c35deb37cca1b95002da2be6c0a810d31b5e3
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-07 20:40:59 -0300

    Add files via upload

---
+++ String_Manipulation_an_code_intelligence.py
@@ -0,0 +1,11 @@
+print("Hello word!\nHello Word")
+print("Hello "+""+"Angela")# space of betwen strings
+
+#String Concatenation 
+
+print("Python"+"learn")
+
+#using the tab space key causes an erro
+   #print("Hello word")
+   
+print("Hello Python")
